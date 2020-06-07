04d7ef8c82c7a210736dcc9699e2f53425d48e51
su8238 <su_1031@naver.com>
2020-06-07 17:22:16 +0900

    initial commit

---
+++ app/src/main/java/com/yujin/foodtruck/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yujin.foodtruck;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
